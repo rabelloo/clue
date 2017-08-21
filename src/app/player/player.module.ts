c6bea2808a1732e5827ace63117d16182004dcfa
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2017-08-21 20:30:46 -0300

    feat(player): initial; and general styling

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MdCardModule, MdInputModule } from "@angular/material";
 
 import { PlayerComponent } from './player.component';
+import { PlayerService } from './player.service';
 
 @NgModule({
   imports: [
-    
+    CommonModule,
+    MdCardModule,
+    MdInputModule
   ],
   declarations: [
     PlayerComponent
-  ]
+  ],
+  providers: [PlayerService]
 })
 export class PlayerModule { }
