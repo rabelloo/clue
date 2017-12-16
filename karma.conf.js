362f5783436525653352b91f3c657eaae2bfabd7
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2017-12-16 00:29:31 -0200

    ci: configure firebase and karma

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['Chrome', 'Chrome_travis_ci'],
+    singleRun: false,
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
   });
+  
 };
