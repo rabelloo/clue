ffe8aea6c9c5adc8f03b1aeb1c5bf6facb7b83af
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2018-01-19 23:54:33 -0200

    chore: lint fix

--- src/hmr.ts
+++ src/hmr.ts
@@ -1,4 +1,4 @@
-﻿import { ApplicationRef } from '@angular/core';
+import { ApplicationRef } from '@angular/core';
 import { ActionReducer } from '@ngrx/store';
 import { createNewHosts, createInputTransfer, removeNgStyles } from '@angularclass/hmr';
 
@@ -33,13 +33,13 @@ export abstract class HotModuleReloadModule {
   hmrOnDestroy(store) {
     const cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
 
-    store.disposeOldHosts = createNewHosts(cmpLocation)
+    store.disposeOldHosts = createNewHosts(cmpLocation);
     store.restoreInputValues = createInputTransfer();
     removeNgStyles();
   }
 
   hmrAfterDestroy(store) {
-    store.disposeOldHosts()
+    store.disposeOldHosts();
     delete store.disposeOldHosts;
   }
 
