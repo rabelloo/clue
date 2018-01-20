eee744e0ef8609ae37cd7f31f9a79087fd102d82
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2018-01-20 00:15:29 -0200

    fix(hmr): export metaReducer function

--- src/hmr.ts
+++ src/hmr.ts
@@ -2,13 +2,14 @@ import { ApplicationRef } from '@angular/core';
 import { ActionReducer } from '@ngrx/store';
 import { createNewHosts, createInputTransfer, removeNgStyles } from '@angularclass/hmr';
 
-export const metaReducers = [
-  (reducer: ActionReducer<any>) =>
-      (state: any, action: any) =>
-          action.type === 'SET_ROOT_STATE'
-              ? action.payload
-              : reducer(state, action)
-];
+export const metaReducers = [ metaReducer ];
+
+export function metaReducer(reducer: ActionReducer<any>) {
+  return (state: any, action: any) =>
+    action.type === 'SET_ROOT_STATE'
+        ? action.payload
+        : reducer(state, action);
+}
 
 export abstract class HotModuleReloadModule {
 
